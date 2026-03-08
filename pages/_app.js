c4bd681d1c067e5a2d3b6aed9b70dbb2dcb67c4e
Aditya Shankar Tripathi <flutterarvr@gmail.com>
2026-03-08 18:04:52 +0530

    google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,4 @@
-/*!
 
-=========================================================
-* NextJS Material Kit v1.2.2 based on Material Kit Free - v2.0.2 (Bootstrap 4.0.0 Final Edition) and Material Kit React v1.8.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/nextjs-material-kit
-* Copyright 2023 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/nextjs-material-kit/blob/main/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import React from "react";
 import App from "next/app";
 import Head from "next/head";
@@ -22,23 +6,7 @@ import "/styles/scss/nextjs-material-kit.scss?v=1.2.0";
 
 export default class MyApp extends App {
   componentDidMount() {
-    let comment = document.createComment(`
-
-=========================================================
-* NextJS Material Kit v1.2.2 based on Material Kit Free - v2.0.2 (Bootstrap 4.0.0 Final Edition) and Material Kit React v1.8.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/nextjs-material-kit
-* Copyright 2023 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/nextjs-material-kit/blob/main/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-`);
+    let comment = document.createComment(``);
     document.insertBefore(comment, document.documentElement);
   }
   static async getInitialProps({ Component, router, ctx }) {
@@ -55,6 +23,18 @@ export default class MyApp extends App {
 
     return (
       <React.Fragment>
+         <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-D95NPS57PD"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-D95NPS57PD');
+          `}
+        </Script>
         <Head>
           <meta
             name="viewport"
